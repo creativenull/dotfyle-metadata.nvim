6da3c5fc9424c8f210bf79e766b3a74e5c5a726e
Rory <ranesbitt@live.co.uk>
2023-07-25 22:34:29 +0100

    feat(keymaps): make lhs and rhs more readable

--- lua/dotfyle_metadata/init.lua
+++ lua/dotfyle_metadata/init.lua
@@ -39,9 +39,16 @@ end
 
 local function get_keymaps()
   local keymaps = vim.api.nvim_get_keymap('')
-  -- TODO: remove the default keymaps
   for _, map in pairs(keymaps) do
+    -- translate the strings into something usable by :map
+    map.lhs = vim.fn.keytrans(map.lhs)
+
+    if map.rhs == "" then
+      -- replace the empty string with empty modifier
+      map.rhs = "<Nop>"
+    end
     if map.callback then
+      -- check if the mapping is a function
       -- HACK: can't encode a function
       -- but we should probably write out what it does
       map.callback = "function"
