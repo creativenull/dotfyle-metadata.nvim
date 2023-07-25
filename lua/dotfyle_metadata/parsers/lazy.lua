f37365f6c4cbd9dd5d23c6f3e39d7b5d462d93fe
Arnold Chand <creativenull@outlook.com>
2023-07-25 13:37:50 -0400

    feat: add lazy.nvim parser

---
+++ lua/dotfyle_metadata/parsers/lazy.lua
@@ -0,0 +1,18 @@
+return {
+	name = "lazy.nvim",
+
+	installed = function()
+		local loaded = pcall(require, "lazy")
+		return loaded
+	end,
+
+	parse = function()
+    local list = {}
+
+    for _, plugin in pairs(require('lazy').plugins()) do
+      table.insert(list, plugin[1])
+    end
+
+    return list
+	end,
+}
