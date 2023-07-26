f900ba1939e3be73524673bc6e8afc88ad55d013
Arnold Chand <creativenull@outlook.com>
2023-07-26 15:01:40 -0400

    fix: simplify data for keymaps

--- lua/dotfyle_metadata/keymaps.lua
+++ lua/dotfyle_metadata/keymaps.lua
@@ -1,8 +1,10 @@
 ---Get all user defined keymaps
 ---@return table
 return function()
-	local keymaps = vim.api.nvim_get_keymap("")
-	for _, map in pairs(keymaps) do
+	local keymaps = {}
+	local global_keymaps = vim.api.nvim_get_keymap("")
+
+	for _, map in pairs(global_keymaps) do
 		-- translate the strings into something usable by :map
 		map.lhs = vim.api.nvim_replace_termcodes(map.lhs, true, true, false)
 
@@ -10,12 +12,22 @@ return function()
 			-- replace the empty string with empty modifier
 			map.rhs = "<Nop>"
 		end
+
 		if map.callback then
 			-- check if the mapping is a function
 			-- HACK: can't encode a function
 			-- but we should probably write out what it does
 			map.callback = "function"
 		end
+
+		table.insert(keymaps, {
+			mode = map.mode,
+			lhs = map.lhs,
+			rhs = map.rhs,
+			desc = map.desc == nil and "" or map.desc,
+			noremap = map.noremap == 1,
+		})
 	end
+
 	return keymaps
 end
