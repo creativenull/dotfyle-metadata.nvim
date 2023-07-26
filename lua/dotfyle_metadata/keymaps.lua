f4bf16f768f64488185f4c87c1f1562a3172066f
Rory <ranesbitt@live.co.uk>
2023-07-26 21:42:10 +0100

    feat(keymaps): if callback replace rhs

--- lua/dotfyle_metadata/keymaps.lua
+++ lua/dotfyle_metadata/keymaps.lua
@@ -8,16 +8,14 @@ return function()
 		-- translate the strings into something usable by :map
 		map.lhs = vim.api.nvim_replace_termcodes(map.lhs, true, true, false)
 
-		if map.rhs == "" then
-			-- replace the empty string with empty modifier
-			map.rhs = "<Nop>"
-		end
-
 		if map.callback then
 			-- check if the mapping is a function
 			-- HACK: can't encode a function
 			-- but we should probably write out what it does
-			map.callback = "function"
+			map.rhs = "<function>"
+    elseif map.rhs == "" then
+			-- replace the empty string with empty modifier
+			map.rhs = "<Nop>"
 		end
 
 		table.insert(keymaps, {
