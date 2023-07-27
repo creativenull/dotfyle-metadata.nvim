9a067d28a7e1ef764f930b380c11b458b9bd5c83
Rory <ranesbitt@live.co.uk>
2023-07-27 16:47:53 +0100

    chore(keymaps): run stylua

--- lua/dotfyle_metadata/keymaps.lua
+++ lua/dotfyle_metadata/keymaps.lua
@@ -2,20 +2,22 @@
 ---@return table
 
 local function check_map(keymap)
-  -- check if the mapping is one we want to include
+	-- check if the mapping is one we want to include
 
-  if  keymap.mode ~= "n" and keymap.mode ~= "i"
-  and keymap.mode ~= "x" and keymap.mode ~= "v"
-  and keymap.mode ~= "t"
-  then
-    return false
-  end
-  if string.find(keymap.lhs, "<Plug>")
-    then
-      return false
-  end
+	if
+		keymap.mode ~= "n"
+		and keymap.mode ~= "i"
+		and keymap.mode ~= "x"
+		and keymap.mode ~= "v"
+		and keymap.mode ~= "t"
+	then
+		return false
+	end
+	if string.find(keymap.lhs, "<Plug>") then
+		return false
+	end
 
-  return true
+	return true
 end
 
 return function()
@@ -31,20 +33,20 @@ return function()
 			-- HACK: can't encode a function
 			-- but we should probably write out what it does
 			map.rhs = "<function>"
-    elseif map.rhs == "" then
+		elseif map.rhs == "" then
 			-- replace the empty string with empty modifier
 			map.rhs = "<Nop>"
 		end
 
-    if check_map(map) then
-      table.insert(keymaps, {
-        mode = map.mode,
-        lhs = map.lhs,
-        rhs = map.rhs,
-        desc = map.desc == nil and "" or map.desc,
-        noremap = map.noremap == 1,
-      })
-    end
+		if check_map(map) then
+			table.insert(keymaps, {
+				mode = map.mode,
+				lhs = map.lhs,
+				rhs = map.rhs,
+				desc = map.desc == nil and "" or map.desc,
+				noremap = map.noremap == 1,
+			})
+		end
 	end
 
 	return keymaps
