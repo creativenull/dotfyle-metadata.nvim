29abd938064cdd95cff3aaf925c86e0aa9b8a32a
Arnold Chand <creativenull@outlook.com>
2023-07-27 13:08:59 -0400

    fix: rm auto fmt, new open file command

--- lua/dotfyle_metadata/init.lua
+++ lua/dotfyle_metadata/init.lua
@@ -5,7 +5,8 @@ local get_servers = require("dotfyle_metadata.extras.lsp")
 local get_mason_tools = require("dotfyle_metadata.extras.mason")
 
 local M = {}
-local dotfyle_path = string.format("%s/dotfyle.json", vim.fn.stdpath("config"))
+
+M.dotfyle_path = string.format("%s/dotfyle.json", vim.fn.stdpath("config"))
 
 function M.generate()
 	local plugin_manager, plugins = get_plugins()
@@ -21,22 +22,9 @@ function M.generate()
 	}
 
 	local json = vim.json.encode(dotfyle_ref)
-	vim.fn.writefile({ json }, dotfyle_path)
+	vim.fn.writefile({ json }, M.dotfyle_path)
 
-	vim.notify("[dotfyle] dotfyle.json generated!", nil, nil)
-	vim.cmd(string.format("edit %s", dotfyle_path))
-
-	-- TODO: Try something else instead of this complexity
-	if vim.fn.executable("jq") == 1 then
-		vim.cmd("%!jq")
-		vim.cmd("write")
-	elseif vim.fn.executable("prettier") == 1 then
-		vim.cmd("%!prettier --no-color --stdin --stdin-filepath %")
-		vim.cmd("write")
-	elseif vim.fn.has("python3") == 1 then
-		vim.cmd("%!python3 -m json.tool")
-		vim.cmd("write")
-	end
+	vim.notify("[dotfyle-metadata] json file generated, open with `:DotfyleOpen`", nil, nil)
 end
 
 return M
--- plugin/dotfyle_metadata.lua
+++ plugin/dotfyle_metadata.lua
@@ -2,7 +2,12 @@ if vim.g.loaded_dotfyle_metadata then
 	return
 end
 
-local dotfyle = require("dotfyle_metadata")
-vim.api.nvim_create_user_command("DotfyleGenerate", dotfyle.generate, { desc = "Generate a dotfyle.json file" })
+local dm = require("dotfyle_metadata")
+vim.api.nvim_create_user_command("DotfyleGenerate", dm.generate, { desc = "Generate a dotfyle.json file" })
+vim.api.nvim_create_user_command(
+	"DotfyleOpen",
+	string.format("<Cmd>edit %s<CR>", dm.dotfyle_path),
+	{ desc = "Open the dotfyle.json file" }
+)
 
 vim.g.loaded_dotfyle_metadata = true
