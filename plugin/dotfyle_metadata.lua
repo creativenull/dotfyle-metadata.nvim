3d6a205432d3174924d06c878b78c31f05db7355
Arnold Chand <creativenull@outlook.com>
2023-07-27 13:10:59 -0400

    fix: no use of modifiers

--- plugin/dotfyle_metadata.lua
+++ plugin/dotfyle_metadata.lua
@@ -6,7 +6,7 @@ local dm = require("dotfyle_metadata")
 vim.api.nvim_create_user_command("DotfyleGenerate", dm.generate, { desc = "Generate a dotfyle.json file" })
 vim.api.nvim_create_user_command(
 	"DotfyleOpen",
-	string.format("<Cmd>edit %s<CR>", dm.dotfyle_path),
+	string.format("edit %s", dm.dotfyle_path),
 	{ desc = "Open the dotfyle.json file" }
 )
 
