68529cb1d16ee795e5ff44a7b8ff34757538fd7e
Rory Nesbitt <ranesbitt@live.co.uk>
2023-07-27 16:50:01 +0100

    improved keymaps detection (#4)

--- lua/dotfyle_metadata/keymaps.lua
+++ lua/dotfyle_metadata/keymaps.lua
@@ -1,32 +1,52 @@
 ---Get all user defined keymaps
 ---@return table
+
+local function check_map(keymap)
+	-- check if the mapping is one we want to include
+
+	if
+		keymap.mode ~= "n"
+		and keymap.mode ~= "i"
+		and keymap.mode ~= "x"
+		and keymap.mode ~= "v"
+		and keymap.mode ~= "t"
+	then
+		return false
+	end
+	if string.find(keymap.lhs, "<Plug>") then
+		return false
+	end
+
+	return true
+end
+
 return function()
 	local keymaps = {}
 	local global_keymaps = vim.api.nvim_get_keymap("")
 
 	for _, map in pairs(global_keymaps) do
 		-- translate the strings into something usable by :map
-		map.lhs = vim.api.nvim_replace_termcodes(map.lhs, true, true, false)
-
-		if map.rhs == "" then
-			-- replace the empty string with empty modifier
-			map.rhs = "<Nop>"
-		end
+		map.lhs = vim.fn.maparg(map.lhsraw, map.mode, false, true).lhs
 
 		if map.callback then
 			-- check if the mapping is a function
 			-- HACK: can't encode a function
 			-- but we should probably write out what it does
-			map.callback = "function"
+			map.rhs = "<function>"
+		elseif map.rhs == "" then
+			-- replace the empty string with empty modifier
+			map.rhs = "<Nop>"
 		end
 
-		table.insert(keymaps, {
-			mode = map.mode,
-			lhs = map.lhs,
-			rhs = map.rhs,
-			desc = map.desc == nil and "" or map.desc,
-			noremap = map.noremap == 1,
-		})
+		if check_map(map) then
+			table.insert(keymaps, {
+				mode = map.mode,
+				lhs = map.lhs,
+				rhs = map.rhs,
+				desc = map.desc == nil and "" or map.desc,
+				noremap = map.noremap == 1,
+			})
+		end
 	end
 
 	return keymaps
