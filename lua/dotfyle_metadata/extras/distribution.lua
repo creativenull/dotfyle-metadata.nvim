cbd50f5347adf97c6785f95616b28f3f709b6802
Sean Boult <996134+Hacksore@users.noreply.github.com>
2024-02-18 09:41:19 -0600

    refactor(extras): simplify the distro logic

--- lua/dotfyle_metadata/extras/distribution.lua
+++ lua/dotfyle_metadata/extras/distribution.lua
@@ -1,29 +1,33 @@
-local Distribution = {
-  AstroNvim = "AstroNvim",
-  NvChad = "NvChad",
-  LunarVim = "LunarVim",
-  Custom = "Custom"
+---The supported distributions
+local supported_distributions = {
+  astro_nvim = {
+    -- module: https://github.com/AstroNvim/AstroNvim/tree/main/lua/astronvim/utils
+    import = "astronvim.utils",
+    name = "AstroNvim"
+  },
+  nv_chad = {
+    -- module: https://github.com/NvChad/NvChad/tree/v2.0/lua/core
+    import = "nvchad.core",
+    name = "NvChad"
+  },
+  lunar_vim = {
+    -- module: https://github.com/LunarVim/LunarVim/tree/master/lua/lvim/core
+    import = "lvim.core",
+    name = "LunarVim"
+  },
 }
 
----Get the nvim distro
+---Get the nvim distro and return the name, or return custom
 ---@return string
 return function()
-  local loaded
-
-  loaded, _ = pcall(require, "astronvim");
-  if loaded then
-    return Distribution.AstroNvim
+  -- iterate over the supported distros and check if the import is found
+  for key, _ in pairs(supported_distributions) do
+    local distro = supported_distributions[key]
+    if pcall(require, distro.import) then
+      return supported_distributions[key].name
+    end
   end
 
-  loaded = pcall(require, "nvchad");
-  if loaded then
-    return Distribution.NvChad
-  end
-
-  loaded = pcall(require, "lunarvim");
-  if loaded then
-    return Distribution.LunarVim
-  end
-
-  return Distribution.Custom
+  -- If no distro is found, return custom
+  return "custom"
 end
