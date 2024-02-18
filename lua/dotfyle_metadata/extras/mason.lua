263425a644ebcdefe85fd8af803a275aff3b9459
Sean Boult <996134+Hacksore@users.noreply.github.com>
2024-02-18 11:33:14 -0600

    refactor: update mason too

--- lua/dotfyle_metadata/extras/mason.lua
+++ lua/dotfyle_metadata/extras/mason.lua
@@ -9,15 +9,18 @@ return function()
 
 	local install_dir = string.format("%s/bin", vim.env.MASON)
 
-  if vim.fn.isdirectory(install_dir) == 0 then
-    return {}
-  end
+	if vim.fn.isdirectory(install_dir) == 0 then
+		return {}
+	end
 
 	local files
 	files = vim.split(vim.fn.globpath(install_dir, "*"), "\n", {})
 	files = vim.tbl_map(function(file)
 		return vim.fn.fnamemodify(file, ":t")
 	end, files)
+
+	-- sort the plugins A-Za-z
+	table.sort(files)
 
 	return files
 end
