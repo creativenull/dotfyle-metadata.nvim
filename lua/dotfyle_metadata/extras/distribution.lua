40dd24903bec823c51469d5071e94c2496288b71
Sean Boult <996134+Hacksore@users.noreply.github.com>
2024-04-02 12:34:50 -0500

    fix(extra/distro): support astronvim v4

--- lua/dotfyle_metadata/extras/distribution.lua
+++ lua/dotfyle_metadata/extras/distribution.lua
@@ -1,33 +1,35 @@
----The supported distributions
 local supported_distributions = {
-  astro_nvim = {
-    -- module: https://github.com/AstroNvim/AstroNvim/tree/main/lua/astronvim/utils
-    import = "astronvim.utils",
-    name = "AstroNvim"
-  },
-  nv_chad = {
-    -- module: https://github.com/NvChad/NvChad/tree/v2.0/lua/core
-    import = "nvchad.core",
-    name = "NvChad"
-  },
-  lunar_vim = {
-    -- module: https://github.com/LunarVim/LunarVim/tree/master/lua/lvim/core
-    import = "lvim.core",
-    name = "LunarVim"
-  },
+	AstroNvim = {
+		is_enabled = function()
+			local astrov3_loaded, _ = pcall(require, "astronvim")
+			local astrov4_loaded, _ = pcall(require, "astrocore")
+
+			return astrov3_loaded or astrov4_loaded
+		end,
+	},
+	NvChad = {
+		is_enabled = function()
+			return pcall(require, "nvchad")
+		end,
+	},
+
+	LunarVim = {
+		is_enabled = function()
+			return pcall(require, "lunarvim")
+		end,
+	},
 }
 
 ---Get the nvim distro and return the name, or return custom
 ---@return string
 return function()
-  -- iterate over the supported distros and check if the import is found
-  for key, _ in pairs(supported_distributions) do
-    local distro = supported_distributions[key]
-    if pcall(require, distro.import) then
-      return supported_distributions[key].name
-    end
-  end
+	-- iterate over the supported distros and check if the import is found
+	for k, distro in pairs(supported_distributions) do
+		if distro.is_enabled() then
+			return k
+		end
+	end
 
-  -- If no distro is found, return custom
-  return "custom"
+	-- If no distro is found, return custom
+	return "custom"
 end
